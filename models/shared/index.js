8f033cbeb029134a05d1ed753b9b5d73687b6649
Mritunjay Kumar <mjdemon211@gmail.com>
2021-07-22 10:48:39 +0530

    Added sub-categories

--- models/shared/index.js
+++ models/shared/index.js
@@ -1,9 +1,11 @@
 const userDb = require('./user.db');
 const categoryDb = require('./category.db');
+const subCategoryDb = require('./subCategory.db');
 const rawQuery = require('./rawQuries');
 
 module.exports = {
   userDb,
   categoryDb,
+    subCategoryDb,
   rawQuery,
 };
