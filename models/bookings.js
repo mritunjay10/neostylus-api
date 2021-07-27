b3f279cb73b432a3bd576423c1d52828714bbee9
Mritunjay Kumar <mjdemon211@gmail.com>
2021-07-28 00:21:37 +0530

    Create booking

--- models/bookings.js
+++ models/bookings.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes)=> {
                 allowNull: false,
             },
             date: {
-                type: DataTypes.DATE,
+                type: DataTypes.DATEONLY,
                 allowNull: false,
             },
             slot: {
