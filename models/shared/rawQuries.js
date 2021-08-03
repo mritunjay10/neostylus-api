6ddadf7e035d66ad200a18228c9950cf4a741ead
Mritunjay Kumar <mjdemon211@gmail.com>
2021-08-03 10:03:45 +0530

    Added slot-count in course

--- models/shared/rawQuries.js
+++ models/shared/rawQuries.js
@@ -2,6 +2,6 @@
 exports.slotCount = (course)=> {
     return  'CAST((SELECT COUNT("slots"."id") FROM "slots"' +
         ` WHERE "slots"."course" = ${course} AND ` +
-        ' "reportedUsers"."status" = true AND ' +
-        ' "reportedUsers"."deleted" = false) AS INT)';
+        ' "slots"."status" = true AND ' +
+        ' "slots"."deleted" = false) AS INT)';
 };
